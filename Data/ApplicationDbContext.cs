83a3ebcc62242c59d830a89064ef065baed70877
Shakhzod19980615 <69627740+Shakhzod19980615@users.noreply.github.com>
2021-04-17 02:43:12 +0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Hotel_Managment2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Managment2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Sex> Sexes { get; set; }
+
+    }
+}
